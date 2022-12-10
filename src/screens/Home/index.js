dc348f8f5496cfca0c8cd147c59fe15803616e72
gustavomoraiss <gustavo.morais1098@gmail.com>
2022-12-09 22:03:57 -0300

    remove class component from project

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import Title from '../../components/Title';
-import TitleClass from '../../components/Title/classComponent';
 
 const Home = () => {
-
-  const [title, setTitle] = useState('initial title')
-
-  useEffect(()=> {
-    setTimeout(() => {
-      setTitle('updated title after timeout')
-    }, 3000)
-  }, [])
 
   return (
     <SafeAreaView>
       <View>
-        <Title text={title} />
         <Title />
-        <TitleClass text='teste'/>
       </View>
     </SafeAreaView>
   );
