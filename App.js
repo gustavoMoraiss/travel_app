c6d8d3d04801cfa0bab66bc491a55c673e4c1ac4
gustavomoraiss <gustavo.morais1098@gmail.com>
2022-12-11 12:57:15 -0300

    navigation setup

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Home from './src/screens/Home';
+import AttractionDetails from './src/screens/AttractionDetails';
+import Gallery from './src/screens/Gallery';
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AttractionDetails" component={AttractionDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/screens/AttractionDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const AttractionsDetails = () => {
+  return (
+    <SafeAreaView>
+      <Text>Attraction Details Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default React.memo(AttractionsDetails);
