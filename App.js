bd75b1ede43ad7cb9c2d3156f86bf244242f3b5f
gustavomoraiss <gustavo.morais1098@gmail.com>
2022-12-11 13:01:36 -0300

    hinding navigation header

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="AttractionDetails" component={AttractionDetails} />
       </Stack.Navigator>
