6e855448d7a19ff79d99d9d7ca78018ebaf96a40
gustavomoraiss <gustavo.morais1098@gmail.com>
2022-12-11 16:54:31 -0300

    building a mainImage from AttractionDetails screens and his styles

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,18 +1,53 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {
+  ImageBackground,
+  SafeAreaView,
+  Text,
+  View,
+  Image,
+  Pressable,
+} from 'react-native';
+import styles from './styles';
 
 const AttractionsDetails = ({route, navigation}) => {
   const {item} = route?.params || {};
-
+  const mainImage = item?.images?.length ? item?.images[0] : null;
   const onBack = () => {
     navigation.goBack();
   };
 
   return (
-    <SafeAreaView>
-      <Text onPress={onBack} style={{margin: 40}}>
-        Bakc to the last Screen
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        style={styles.mainImage}
+        imageStyle={{borderRadius: 20}}
+        source={{uri: mainImage}}>
+        <View style={styles.header}>
+          <Pressable onPress={onBack}>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/back.png')}
+            />
+          </Pressable>
+          <Pressable>
+            <Image
+              style={styles.icon}
+              source={require('../../assets/share.png')}
+            />
+          </Pressable>
+        </View>
+        <View style={styles.footer}>
+          {item?.images?.length
+            ? item?.images?.map(image => (
+                <Image
+                  key={image}
+                  source={{uri: image}}
+                  style={styles.miniImage}></Image>
+              ))
+            : null}
+        </View>
+      </ImageBackground>
+
       <Text>{item?.name}</Text>
     </SafeAreaView>
   );
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -1,5 +1,45 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
-const styles = StyleSheet.create({});
+const {height} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  mainImage: {
+    width: '100%',
+    height: height / 2,
+    borderRadius: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  container: {
+    margin: 32,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 36,
+    height: 36,
+    margin: 16,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    backgroundColor: 'rgba( 256, 256, 256, 0.35)',
+    margin: 16,
+    paddingHorizontal: 8,
+  },
+  miniImage: {
+    width: 40,
+    height: 40,
+    margin: 8,
+    borderRadius: 10,
+  },
+});
 
 export default styles;
